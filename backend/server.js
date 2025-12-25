6dc3ff3dcf77e2e2f1b4acbcc9ad27fbe97ec4d8
WEBUSMAN <uchohan804@gmail.com>
2025-12-25 16:15:35 +0500

    Update CORS and server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import productRoute from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRoute from "./routes/orderRoute.js";
 
+
 // // Load environment variables
 dotenv.config();
 
@@ -19,6 +20,13 @@ connectCloudinary();
 
 // MIDDLEWARES
 app.use(express.json());
+
+// CORS SETUP
+app.use(cors({
+  origin: ["https://forever-ecommerce-inky.vercel.app"], // allow your frontend
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 app.use(cors());
 
 // API ENDPOINTS
